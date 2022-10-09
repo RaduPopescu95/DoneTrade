0bffd0293b110c0a8fcda905030e610db0cc51a8
Radu <radu.p1995@yahoo.com>
2022-10-09 22:31:52 +0300

    Version 9.2

--- app/components/forms/ProfileEditor.js
+++ app/components/forms/ProfileEditor.js
@@ -9,6 +9,7 @@ import {
   TouchableHighlight,
   Image,
   Alert,
+  TouchableOpacity,
 } from "react-native";
 import colors from "../../config/colors";
 
@@ -23,10 +24,17 @@ function ProfileEditor({
 }) {
   const option = route.params;
   const [focus, setFocus] = useState(false);
+  const [focusConfirm, setFocusConfirm] = useState(false);
   const [opt, setOpt] = useState(option.subtitle);
   // const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
 
-  const handleFocus = () => setFocus(!focus);
+  const handleFocusConfirmPass = () => {
+    setFocusConfirm(!focusConfirm);
+  };
+
+  const handleFocus = () => {
+    setFocus(!focus);
+  };
   useEffect(() => {
     navigation.setOptions({ headerTitle: option.title });
     console.log("options...", option.title);
@@ -34,8 +42,10 @@ function ProfileEditor({
   return (
     <>
       <View style={styles.container}>
+        <View style={styles.textPlaceHolder}>
+          <Text style={styles.txt}>{option.title}</Text>
+        </View>
         <View style={styles.inputContainer}>
-          <Text>asdas</Text>
           <TextInput
             onFocus={handleFocus}
             onBlur={handleFocus}
@@ -46,10 +56,11 @@ function ProfileEditor({
                 // borderBottomColor: colors.secondary,
                 flex: 1,
                 borderBottomWidth: 1,
+                width: "100%",
               },
               {
                 borderBottomColor: !focus ? colors.medium : colors.dark,
-                borderBottomWidth: !focus ? 1 : 2,
+                borderBottomWidth: !focus ? 1 : 2.5,
               },
             ]}
             placeholder={
@@ -63,8 +74,8 @@ function ProfileEditor({
         {option.title === "Change password" && (
           <View style={styles.inputContainer}>
             <TextInput
-              onFocus={handleFocus}
-              onBlur={handleFocus}
+              onFocus={handleFocusConfirmPass}
+              onBlur={handleFocusConfirmPass}
               style={[
                 {
                   height: 45,
@@ -72,10 +83,13 @@ function ProfileEditor({
                   // borderBottomColor: colors.secondary,
                   flex: 1,
                   borderBottomWidth: 1,
+                  width: "100%",
                 },
                 {
-                  borderBottomColor: !focus ? colors.medium : colors.dark,
-                  borderBottomWidth: !focus ? 1 : 2,
+                  borderBottomColor: !focusConfirm
+                    ? colors.medium
+                    : colors.dark,
+                  borderBottomWidth: !focusConfirm ? 1 : 2.5,
                 },
               ]}
               placeholder="Confirm password"
@@ -86,6 +100,15 @@ function ProfileEditor({
           </View>
         )}
       </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          // style={[styles.button, { backgroundColor: colors[color] }]}
+          style={styles.button}
+          // onPress={onPress}
+        >
+          <Text style={styles.text}>Save</Text>
+        </TouchableOpacity>
+      </View>
       {/* <View style={styles.containerButton}>
       </View> */}
     </>
@@ -93,6 +116,22 @@ function ProfileEditor({
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    // alignContent: "flex-end",
+    padding: 15,
+    width: "80%",
+    marginVertical: 20,
+  },
+  text: {
+    color: colors.white,
+    fontSize: 18,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
   container: {
     flex: 3,
     justifyContent: "flex-start",
@@ -109,11 +148,24 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     marginBottom: 20,
   },
-  logo: {
-    width: 120,
-    height: 120,
-    justifyContent: "center",
-    marginBottom: 20,
+  textPlaceHolder: {
+    width: "100%",
+
+    fontSize: 20,
+    // borderColor: colors.dark,
+    backgroundColor: colors.light,
+    // borderRadius: 30,
+    // borderBottomWidth: 1,
+
+    height: 17,
+    marginTop: 20,
+    paddingLeft: 30,
+    marginLeft: 20,
+  },
+  txt: {
+    color: colors.dark,
+    fontSize: 15,
+    fontWeight: "bold",
   },
   inputContainer: {
     // borderColor: colors.dark,
@@ -122,8 +174,8 @@ const styles = StyleSheet.create({
     // borderBottomWidth: 1,
     width: 300,
     height: 45,
-    marginTop: 40,
-    flexDirection: "row",
+    marginTop: 0,
+    flexDirection: "column",
     // flex: 1
     alignItems: "center",
   },
@@ -148,13 +200,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   buttonContainer: {
-    height: 45,
-    flexDirection: "row",
+    // height: 45,
+    // flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 20,
-    width: "80%",
-    borderRadius: 30,
+    paddingBottom: 20,
+    // width: "80%",
+    // borderRadius: 30,
+    backgroundColor: colors.light,
   },
   sendButton: {
     marginTop: 30,
